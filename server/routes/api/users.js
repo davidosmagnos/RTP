99ea65f3c7714348d9217b593b335bc428746fbe
davidosmagnos <davemagno99@gmail.com>
2022-09-05 15:13:26 +0800

    add users to dashboard page

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -26,13 +26,30 @@ router.get("/",(req,res)=>{
     )
 })
 router.get("/:username",(req,res)=>{
-    var data;
     con.query(
         `select 'first_name',first_name,'last_name',last_name,'username',username,'password',password FROM users where username = '${req.params.username}'`,
         (err,results)=>{
             if(err) res.send(err);
             res.send(results)
-            
+        }
+    )
+})
+
+router.post('/',(req,res)=>{
+    con.query(
+        `insert into users(username,password,first_name,last_name)values('${req.body.username}','${req.body.password}','${req.body.first_name}','${req.body.last_name}')`,
+        (err,results)=>{
+            if(err) throw err;
+            res.status(201).send()
+        }
+    )
+})
+router.delete('/:id',(req,res)=>{
+    con.query(
+        `delete from users where username = '${req.params.id}'`,
+        (err,results)=>{
+            if(err) throw err;
+            res.status(200).send()
         }
     )
 })
